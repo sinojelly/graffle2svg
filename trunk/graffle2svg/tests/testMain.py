b69f4bc9b850cba66804ee9ef3e7f22e92171d24
timwintle <timwintle@2d509eb4-8272-11de-bbe9-c3d9379d45d0>
2011-04-14 16:46:32 +0000

    Updating unit tests as per jnavila's fork to keep merging easier

--- trunk/graffle2svg/tests/testMain.py
+++ trunk/graffle2svg/tests/testMain.py
@@ -5,19 +5,19 @@ import main
 class TestMkHex(TestCase):
 
     def testZero(self):
-        assert main.mkHex(0.0) == "00"
+        self.assertEqual(main.mkHex(0.0),"00")
 
     def testFull(self):
-        assert main.mkHex(1.0) == "ff"
+        self.assertEqual(main.mkHex(1.0) , "ff")
 
     def testOne(self):
-        assert main.mkHex(1.0/255) == "01"
+        self.assertEqual(main.mkHex(1.0/255) , "01")
 
     def testFifteen(self):
-        assert main.mkHex(15.0/255) == "0f"
+        self.assertEqual(main.mkHex(15.0/255) , "0f")
 
     def test254(self):
-        assert main.mkHex(254.0/255) == "fe"
+        self.assertEqual(main.mkHex(254.0/255) , "fe")
 
 def get_tests():
     TS = TestSuite()
