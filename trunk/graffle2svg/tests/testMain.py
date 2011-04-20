80f7d0b598a0452dc2e6cef5a2d171a01826afea
timwintle <timwintle@2d509eb4-8272-11de-bbe9-c3d9379d45d0>
2011-04-20 11:21:12 +0000

    (jnavila) correcting integer misspelling and adding unit tests for parsing types

--- trunk/graffle2svg/tests/testMain.py
+++ trunk/graffle2svg/tests/testMain.py
@@ -1,6 +1,7 @@
 
 from unittest import makeSuite, TestCase, TestSuite
 import main
+import xml.dom.minidom
 
 class TestMkHex(TestCase):
 
@@ -19,7 +20,31 @@ class TestMkHex(TestCase):
     def test254(self):
         self.assertEqual(main.mkHex(254.0/255) , "fe")
 
+class TestGraffleParser(TestCase):
+    def setUp(self):
+        self.gp = main.GraffleParser()
+    
+    def tearDown(self):
+        del self.gp
+
+    def testGraffleDictInteger(self):
+        p = xml.dom.minidom.parseString("<dict><key>ImageCounter</key><integer>1</integer></dict>")
+        dict = self.gp.ReturnGraffleDict(p.firstChild)
+        self.assertEqual(dict['ImageCounter'], '1')
+ 
+    def testGraffleDictReal(self):
+        p = xml.dom.minidom.parseString("<dict><key>Size</key><real>19</real></dict>")
+        dict = self.gp.ReturnGraffleDict(p.firstChild)
+        self.assertEqual(dict['Size'], '19')
+
+    def testGraffleDictReal(self):
+        p = xml.dom.minidom.parseString("<dict><key>Shape</key><string>RoundRect</string></dict>")
+        dict = self.gp.ReturnGraffleDict(p.firstChild)
+        self.assertEqual(dict['Shape'], 'RoundRect')
+
+
 def get_tests():
     TS = TestSuite()
     TS.addTest(makeSuite(TestMkHex))
+    TS.addTest(makeSuite(TestGraffleParser))
     return TS
